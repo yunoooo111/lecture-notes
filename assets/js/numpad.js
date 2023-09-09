cbe8cbf3057f450db52656091c871368fe267305
unknown <dudung@gmail.com>
2023-09-09 14:26:45 +0700

    fin

--- assets/js/numpad.js
+++ assets/js/numpad.js
@@ -1,13 +1,10 @@
 /*
-numpad.js
-Create numpad with button and a text area
+  numpad.js
+  Create numpad with button and a text area
 
-Sparisoma Viridi | https://github.com/dudung
+  Sparisoma Viridi | https://github.com/dudung
 
-20230909
-1045 Start this simple app as part of Hugo site.
+  20230909 Start this simple app as part of Hugo site.
 */
 
-function hello() {
-	console.log("Hello, World!");
-}
+
