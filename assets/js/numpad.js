27a4f15873be1c4228d6817202514f03e8f33bea
unknown <dudung@gmail.com>
2023-09-09 11:09:48 +0700

    new js

---
+++ assets/js/numpad.js
@@ -0,0 +1,13 @@
+/*
+numpad.js
+Create numpad with button and a text area
+
+Sparisoma Viridi | https://github.com/dudung
+
+20230909
+1045 Start this simple app as part of Hugo site.
+*/
+
+function hello() {
+	console.log("Hello, World!");
+}
